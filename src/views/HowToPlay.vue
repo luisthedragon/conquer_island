a707594534d7d6c8dc1aa14bebf5906abcb6871b
lg-dragon <123107428+lg-dragon@users.noreply.github.com>
2024-04-30 01:03:25 -0500

    add HowToPlay page

---
+++ src/views/HowToPlay.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container>
+    <h1>Conquer Island</h1>
+    <p>
+      In the heart of a remote island, a silent war rages between
+      <b>dogs</b> and <b>lions</b>. The island's lush foliage hides a secret
+      struggle for dominance. <b>Your mission:</b> to conquer the island by
+      <b>eliminating all lion presence.</b>
+    </p>
+    <h2>Gameplay</h2>
+    <p>
+      Engage in this enigmatic conflict with a simple mechanic:
+      <b>click tiles to swap between dog and lion forms.</b> Each move is
+      strategic, as you navigate the shifting balance of power. Outmaneuver the
+      lions with cunning strategy and unwavering resolve to claim victory and
+      secure the island for your canine brethren.
+    </p>
+  </b-container>
+</template>
+
+<style scoped>
+h1,
+h2 {
+  color: #9fff00;
+}
+</style>
